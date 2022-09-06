01b56446f17bb4a65124af930734c7c292ed214c
Jonathan Lindqvist <91454543+ocmino@users.noreply.github.com>
2022-09-06 12:33:19 +0200

    Add files via upload

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,34 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "../UI/ExpenseFilter";
+import React, { useState } from "react";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
---
+++ src/components/UI/Logo.js
@@ -0,0 +1,15 @@
+//create logo component
+
+import React from 'react';
+
+import './Logo.css';
+
+const Logo = () => {
+    return (
+        <div className='logo'>
+            <h1>Expense Tracker</h1>
+        </div>
+    );
+}
+
+export default Logo;
